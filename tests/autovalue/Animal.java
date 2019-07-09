f91bf45caf9b0286c1a569da657da9a3dbc1a116
Manu Sridharan <msridhar@gmail.com>
2019-07-08 21:39:31 -0700

    Fix handling of overrides with generated code

--- tests/autovalue/Animal.java
+++ tests/autovalue/Animal.java
@@ -1,9 +1,16 @@
-import org.checkerframework.checker.returnsrcvr.qual.*;
 import com.google.auto.value.AutoValue;
+import org.checkerframework.checker.nullness.qual.*;
 
+/**
+ * Adapted from the standard AutoValue example code:
+ * https://github.com/google/auto/blob/master/value/userguide/builders.md
+ */
 @AutoValue
 abstract class Animal {
   abstract String name();
+
+  abstract @Nullable String habitat();
+
   abstract int numberOfLegs();
 
   static Builder builder() {
@@ -12,8 +19,42 @@ abstract class Animal {
 
   @AutoValue.Builder
   abstract static class Builder {
+
     abstract Builder setName(String value);
+
     abstract Builder setNumberOfLegs(int value);
+
+    abstract Builder setHabitat(String value);
+
     abstract Animal build();
+  }
+
+  public static void buildSomethingWrong() {
+    Builder b = builder();
+    b.setName("Frank");
+    b.build();
+  }
+
+  public static void buildSomethingRight() {
+    Builder b = builder();
+    b.setName("Frank");
+    b.setNumberOfLegs(4);
+    b.build();
+  }
+
+  public static void buildSomethingRightIncludeOptional() {
+    Builder b = builder();
+    b.setName("Frank");
+    b.setNumberOfLegs(4);
+    b.setHabitat("jungle");
+    b.build();
+  }
+
+  public static void buildSomethingWrongFluent() {
+    builder().setName("Frank").build();
+  }
+
+  public static void buildSomethingRightFluent() {
+    builder().setName("Jim").setNumberOfLegs(7).build();
   }
 }
